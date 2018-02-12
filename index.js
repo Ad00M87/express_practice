ba23229984c834a694b4094516136a08c106473a
Adam Kimball <adamchasekimball@gmail.com>
2018-02-11 20:59:59 -0700

    adding products

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var fs = require('fs');
+var ejs = require('ejs');
 var app = express();
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/products', function(req, res) {
+  fs.readFile('products.json', 'utf8', function(err, data) {
+    var products = JSON.parse(data);
+    res.locals = { products: products };
+    res.render('products.ejs');
+  });
 });
 
 app.listen(8000);
